c0c9242d77a2a7228a6727d447550de7eaf1dbe9
Josh Emerson <josh@zeebox.com>
2014-03-02 16:30:07 +0000

    Fix update_status params

--- pibell.py
+++ pibell.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-import sys
 from twython import Twython
 import config
 
-api = Twython(config.api_key,config.api_secret,config.access_key,config.access_secret) 
+api = Twython(config.api_key,config.api_secret,config.access_key,config.access_secret)
 
-api.update_status('I\'m alive!')
+api.update_status(status='I\'m alive!')
