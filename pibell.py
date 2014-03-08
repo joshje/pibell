ab24441aafe0e9c43bdcbd5269311c352ee06103
Josh Emerson <josh.j.emerson@gmail.com>
2014-03-08 16:51:38 +0000

    Add LED to show activity, button to shutdown

--- pibell.py
+++ pibell.py
@@ -2,14 +2,19 @@
 from twython import Twython
 import RPi.GPIO as GPIO
 import time
+import os
+import sys
 import random
 import config
 
-# Doorbell pin
 buttonPin = 17
+haltPin = 4
+ledPin = 22
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin, GPIO.IN)
+GPIO.setup(haltPin, GPIO.IN)
+GPIO.setup(ledPin, GPIO.OUT)
 twitter = Twython(config.api_key,config.api_secret,config.access_key,config.access_secret)
 
 messages = [
@@ -21,17 +26,29 @@ messages = [
     'are you going to get that or shall I?'
 ]
 
-def getStatusMessage():
+def sendStatusMessage():
     status = ''
     if (config.mentions): status += ' '.join(config.mentions) + ' '
     status += random.choice(messages)
     if (config.debug): print status
     else: twitter.update_status(status=status)
 
-lastPressed = 0
-while True:
-    now = time.time()
-    if (GPIO.input(buttonPin) and (now - lastPressed > config.minInt)):
-        lastPressed = now
-        getStatusMessage()
-    time.sleep(0.05)
+wasPressed = GPIO.input(buttonPin)
+
+try:
+    while True:
+	if (GPIO.input(haltPin)):
+            print 'shutting down'
+            GPIO.cleanup()
+            if not config.debug: os.system('sudo halt')
+            sys.exit()
+        isPressed = GPIO.input(buttonPin)
+        if ((not wasPressed) and isPressed):
+            sendStatusMessage()
+	    GPIO.output(ledPin, True)
+	    time.sleep(5)
+	    GPIO.output(ledPin, False)
+        wasPressed = isPressed
+        time.sleep(0.05)
+except KeyboardInterrupt:
+    GPIO.cleanup()
